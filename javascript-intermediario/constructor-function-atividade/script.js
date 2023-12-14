afc73fc5078ae82e3beb302abf957f8f70d5643b
Leonardo Campos <leonardo.kiss66@live.com>
2023-12-14 19:10:17 -0300

    update

--- javascript-intermediario/constructor-function-atividade/script.js
+++ javascript-intermediario/constructor-function-atividade/script.js
@@ -1,17 +1,17 @@
 // Transforme o objeto abaixo em uma Constructor Function
-function Pessoa(nomeAtribuida,idadeAtribuida) {
+function Pessoa(nomeAtribuida, idadeAtribuida) {
   this.nome = nomeAtribuida; // o nome vai ser o nome que foi atribuida no argumento
   this.idade = idadeAtribuida; // a idade vai ser a idade que foi atribuida no argumento
-  this.andar = function(){
-    console.log(this.nome + ' andou');
-  }
-};
+  this.andar = function () {
+    console.log(this.nome + " andou");
+  };
+}
 
 // Crie 3 pessoas, João - 20 anos,
 // Maria - 25 anos, Bruno - 15 anos
-const joao = new Pessoa('João', '20 anos');
-const maria = new Pessoa('Maria', '25 anos');
-const bruno = new Pessoa('Bruno', '15 anos');
+const joao = new Pessoa("João", "20 anos");
+const maria = new Pessoa("Maria", "25 anos");
+const bruno = new Pessoa("Bruno", "15 anos");
 
 // Crie uma Constructor Function (Dom) para manipulação
 // de listas de elementos do dom. Deve conter as seguintes
@@ -21,14 +21,25 @@ const bruno = new Pessoa('Bruno', '15 anos');
 // addClass(classe), adiciona a classe a todos os elementos
 // removeClass(classe), remove a classe a todos os elementos
 
-function Dom(seletor){
-  const element = document.querySelectorAll(seletor);
-  element.forEach((item)=>{
-    this.ativo = function(classe){
-      item.classList.add(classe);
-    }
-  });
+function Dom(seletor) {
+  const elementList = document.querySelectorAll(seletor);
+  this.elements = elementList;
+  this.addClass = function (classe) {
+    elementList.forEach((element) => {
+      element.classList.add(classe);
+    });
+  };
+
+  this.removeClass = function(classe) {
+    elementList.forEach((element) => {
+      element.classList.remove(classe);
+    })
+  }
 }
 
-const lista = new Dom('li');
-lista.ativo('ativo');
+const listaItens = new Dom("li");
+listaItens.addClass("ativar");
+//listaItens.removeClass("ativar");
+
+const testeUl = new Dom("ul");
+testeUl.addClass("ativar-ul");
