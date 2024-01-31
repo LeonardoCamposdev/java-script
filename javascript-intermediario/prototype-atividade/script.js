00a686617172bbcafb73dadcf1f3847269014962
Leonardo Campos <Leonardocamposdev777@gmail.com>
2024-01-31 19:40:52 -0300

    update

--- javascript-intermediario/prototype-atividade/script.js
+++ javascript-intermediario/prototype-atividade/script.js
@@ -2,18 +2,18 @@
 // Deve conter nome, sobrenome e idade
 // Crie um método no protótipo que retorne
 // o nome completo da pessoa
-function Pessoa(nomeAtribuido,sobrenomeAtribuido,idadeAtribuido){
+function Pessoas(nomeAtribuido,sobreAtribuido,idadeAtribuida){
   this.nome = nomeAtribuido;
-  this.sobrenome = sobrenomeAtribuido;
-  this.idade = idadeAtribuido;
+  this.sobrenome = sobreAtribuido;
+  this.idade = idadeAtribuida;
 }
 
-Pessoa.prototype.nomeCompleto = function(){
+Pessoas.prototype.nomeCompleto = function(){
   return this.nome + this.sobrenome;
 }
 
-const leo = new Pessoa('Leonardo', ' Campos', 24);
-const ravi = new Pessoa('Ravi Mayk', ' Muniz', 4);
+const leo = new Pessoas('Leonardo', ' Campos', 24);
+const ravi = new Pessoas('Ravi Mayk ', 'Muniz', 5);
 
 // Liste os métodos acessados por 
 // dados criados com NodeList,
@@ -21,18 +21,17 @@ const ravi = new Pessoa('Ravi Mayk', ' Muniz', 4);
 console.log(NodeList.prototype);
 console.log(HTMLAllCollection.prototype);
 console.log(Document.prototype);
+
 // Liste os construtores dos dados abaixo
 const li = document.querySelector('li');
 
-li; // 'HTMLLIElement'
-li.click; // 'Function'
-li.innerText; // 'String'
-li.value; // 'Number'
-li.hidden; //  'Boolean'
-li.offsetLeft; // 'Number'
+li; // HTMLLIElement
+li.click; // Function
+li.innerText; // String
+li.value; // Number
+li.hidden; // Boolean
+li.offsetLeft; // Number
 li.click(); // undefined
 
 // Qual o construtor do dado abaixo:
-li.hidden.constructor.name; // retonar uma string !!!
-// 'Boolean'
-// porém o construtor do .name é uma string
+li.hidden.constructor.name; // String
---
+++ javascript-intermediario/string/script.js
@@ -0,0 +1,10 @@
+const comida = 'Pizza';
+const agua = new String('Água');
+
+console.log(comida.length);
+console.log(agua.length);
+
+const frase = 'A melhor comida';
+
+console.log(frase[frase.length - 1]);
+console.log(frase.charAt(2));
