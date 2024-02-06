2e14d2e0e199d2416bb69c5efc722fe89131cd53
Leonardo Campos <Leonardocamposdev777@gmail.com>
2024-02-06 08:04:43 -0300

    update

--- javascript-intermediario/string-atividade/script.js
+++ javascript-intermediario/string-atividade/script.js
@@ -19,20 +19,40 @@ const transacoes = [
     valor: " R$ 129",
   },
   {
-    descricao: "Recebimento de Cliente",
+    descricao: "Recebimento de Taxa Cliente",
     valor: " R$ 49",
   },
 ];
 
-transacoes.forEach((string) => {
-  const fraseCompleta = string.descricao.concat(string.valor);
-  console.log(fraseCompleta);
+let taxaTotal = 0;
+let recebimentoTotal = 0;
+transacoes.forEach((item) => {
+  const numeroLimpo = +item.valor.replace("R$ ", "");
+
+  if (item.descricao.slice(0, 4) === "Taxa") {
+    taxaTotal += numeroLimpo;
+  } else if (item.descricao.slice(0, 4) === "Rece") {
+    recebimentoTotal += numeroLimpo;
+  }
 });
+console.log(taxaTotal);
+console.log(recebimentoTotal);
+
+// no número limpo ele está limpando o R$, para que o número fique puro (NUMBER)
+// e não como uma string, então COM O REPLACE tudo que for R$, ele vai substituir por nada
+// depois temos que colocar o (+) na frente, para que seja transformado
+// essa string em (UM NUMBER).
+// depois ele vai verificar se a descrição começa (for igual) a ("TAXA")
+// se realmente começar ele vai pegar a taxaTotal vai começar um loop
+// com o taxaTotal + o número limpo
+// e no final também vamos verificar o valor do recebimentoTotal
+// usando o else if e usando o SLICE com o === "Rece"
+// só iremos verificar se for o começo da descriação for igual a "RECE"
 
 // Retorne uma array com a lista abaixo
 const transportes = "Carro;Avião;Trem;Ônibus;Bicicleta";
 
-const transportesArray = transportes.split(" ");
+const transportesArray = transportes.split(";");
 console.log(transportesArray);
 
 // Substitua todos os span's por a's
@@ -59,3 +79,15 @@ const transacoes2 = [
   "TARIFA especial",
 ];
 
+let taxasTotal = 0;
+transacoes2.forEach((item) => {
+  item = item.toLocaleLowerCase();
+  item = item.trim();
+  item = item.slice(0, 4);
+
+  if (item === "taxa") {
+    taxasTotal++;
+  }
+});
+
+console.log(taxasTotal);
