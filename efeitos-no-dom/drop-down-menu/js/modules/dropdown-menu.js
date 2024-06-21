50f2ed14d1161deb299b3822d74ee5435fee4e76
Leonardo Campos <Leonardocamposdev777@gmail.com>
2024-06-21 00:16:30 -0300

    update

--- efeitos-no-dom/drop-down-menu/js/modules/dropdown-menu.js
+++ efeitos-no-dom/drop-down-menu/js/modules/dropdown-menu.js
@@ -1,14 +1,22 @@
-export default function initDropdownMenu(){
+import outsideClick from './outsideClick.js'
+export default function initDropdownMenu() {
+  const dropdownMenus = document.querySelectorAll("[data-dropdown"); //NODELIST
 
+  dropdownMenus.forEach((menu) => {
+    ["touchstart", "click"].forEach((userEvent) => {
+      //PARA USAR MAIS DE UM TIPO DE EVENTO NESSE MENU!!!!
+      menu.addEventListener(userEvent, handleCLick);
+    });
+  });
+  
+  function handleCLick(event) {
+    event.preventDefault();
+    this.classList.add("active"); // FAZ REFERÊNCIA AO MENU-DROPDOWN!!!!
+    outsideClick(this, ["touchstart", "click"], () => { //O (THIS) FAZENDO REFERENCIA O ELEMENTO E O (CALLBACK), DO HANDLEOUTSIDECLICK ESSE () => É O CALLBACK VAZIO
+      this.classList.remove('active')
+    }); // TODA VEZ QUE O (HANDLECLICK) OCORRER O CALLBACK (OUTSIDECLICK) VAI SER ATIVADA
+  }
+  
+  
 }
 
-const dropdownMenus = document.querySelectorAll("[data-dropdown");
-
-function handleCLick(event){
-  event.preventDefault();
-  this.classList.toggle("ativo");
-}
-
-dropdownMenus.forEach((menu) =>{
-  menu.addEventListener("click", handleCLick);
-})
