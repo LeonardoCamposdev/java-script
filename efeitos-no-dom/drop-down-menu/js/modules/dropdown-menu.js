33f8d63d9492f1e057167847b2ef8c745d1a4a39
Leonardo Campos <Leonardocamposdev777@gmail.com>
2024-06-24 13:55:27 -0300

    update

--- efeitos-no-dom/drop-down-menu/js/modules/dropdown-menu.js
+++ efeitos-no-dom/drop-down-menu/js/modules/dropdown-menu.js
@@ -1,22 +1,23 @@
-import outsideClick from './outsideClick.js'
+import outsideCLick from "./outsideClick.js";
+
 export default function initDropdownMenu() {
   const dropdownMenus = document.querySelectorAll("[data-dropdown"); //NODELIST
 
-  dropdownMenus.forEach((menu) => {
-    ["touchstart", "click"].forEach((userEvent) => {
-      //PARA USAR MAIS DE UM TIPO DE EVENTO NESSE MENU!!!!
-      menu.addEventListener(userEvent, handleCLick);
+  if (dropdownMenus.length) {
+    dropdownMenus.forEach((menu) => {
+      ["touchstart", "click"].forEach((userEvent) => {
+        //PARA USAR MAIS DE UM TIPO DE EVENTO NESSE MENU!!!!
+        menu.addEventListener(userEvent, handleCLick);
+      });
     });
-  });
-  
-  function handleCLick(event) {
-    event.preventDefault();
-    this.classList.add("active"); // FAZ REFERÊNCIA AO MENU-DROPDOWN!!!!
-    outsideClick(this, ["touchstart", "click"], () => { //O (THIS) FAZENDO REFERENCIA O ELEMENTO E O (CALLBACK), DO HANDLEOUTSIDECLICK ESSE () => É O CALLBACK VAZIO
-      this.classList.remove('active')
-    }); // TODA VEZ QUE O (HANDLECLICK) OCORRER O CALLBACK (OUTSIDECLICK) VAI SER ATIVADA
-  }
-  
-  
-}
 
+    function handleCLick(event) {
+      event.preventDefault();
+      this.classList.add("active");
+
+      outsideCLick(this, ["touchstart", "click"], () => {
+        this.classList.remove("active");
+      });
+    }
+  }
+}
--- efeitos-no-dom/drop-down-menu/js/modules/outsideClick.js
+++ efeitos-no-dom/drop-down-menu/js/modules/outsideClick.js
@@ -1,25 +1,21 @@
-export default function outsideClick(element, events, callback) {   //FUNÇÃO PARA QUANDO CLICARMOS NO HTML (FORA DO MENU), O MENU FECHAR!!!
-  const html = document.documentElement; //SELECIONANDO O HTML
-  const outside = 'data-outside'
+export default function outsideCLick(element, events, callback) {
+  const html = document.documentElement;
+  const outside = "data-outside";
 
-  if(!element.hasAttribute(outside)){ //SE NÃO TIVER O ATRIBUTO OUTSIDE NO THIS, VAI ATIVAR APENAS UMA VEZ, SE TIVER NÃO VAI ADICIONAR 
-    events.forEach(userEvent =>{
-      html.addEventListener(userEvent, handleOutsideClick); // USEREVENT, É A ARRAY COM OS EVENTOS DE CLICK E TOUCHSCREEN
-    })
-    element.setAttribute(outside, '');
+  if (!element.hasAttribute(outside)) {
+    events.forEach((userEvent) => {
+      html.addEventListener(userEvent, handleOutsideClick);
+    });
+    element.setAttribute(outside, "");
   }
 
   function handleOutsideClick(event) {
-    if(!element.contains(event.target)){ // SE ESSA CONDIÇÃO FOR FALSA, VAI EXECUTAR O CALLBACK!!!!, SE FOR TRUE NÃO VAI ACONTECER
-      element.removeAttribute(outside, '');
-      events.forEach(userEvent =>{
-        html.removeEventListener("click", handleOutsideClick); // PARA REMOVER O EVENTO NO HTML!!
-      })
-      callback(); //ATIVANDO A FUNÇÃOA DE CALLBACK DO (HANDLEOUTSIDECLICK) (CALLBACK VAZIO!!)
+    if (!element.contains(event.target)) {
+      element.removeAttribute(outside);
+      events.forEach((userEvent) => {
+        html.removeEventListener(userEvent, handleOutsideClick);
+      });
+      callback();
     }
-    
   }
 }
-
-// O TRUE É SE A GENTE TIVER CLICKANDO DENTRO DO MENU!! AI DIZ QUE O TARGET É TRUE PORQUE ESTAMOS CLIKANDO DENTRO DELE
- // SE ESSA CONDIÇÃO FOR FALSA, VAI EXECUTAR O CALLBACK!!!!, SE FOR TRUE NÃO VAI ACONTECER, (SE O CLICK FOR NO HTML, O MENU VAI SE FECHAR )
