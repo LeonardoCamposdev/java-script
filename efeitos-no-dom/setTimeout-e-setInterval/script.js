a84246e9279d39f62b94c73e6da6b21f290b8ab0
Leonardo Campos <Leonardocamposdev777@gmail.com>
2024-07-07 14:10:14 -0300

    update

--- efeitos-no-dom/setTimeout-e-setInterval/script.js
+++ efeitos-no-dom/setTimeout-e-setInterval/script.js
@@ -14,3 +14,23 @@ for(let i = 0; i < 20; i++){
     console.log(i);
   },300 * i);
 }
+
+const contarAte10 = setInterval(callback,600);
+
+let i = 0;
+function callback(){
+  console.log(i++)
+  if(i< 20){
+    clearInterval(contarAte10);
+  }
+}
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", handleClick);
+
+function handleClick(){
+  setTimeout(() =>{
+    console.log(this);
+    this.classList.add("brelele");
+  },2000)
+}
