d1cb627fce01e79a61bf278321293689c438398f
Leonardo Campos <Leonardocamposdev777@gmail.com>
2024-09-12 22:48:21 -0300

    up

--- javascript-assincrono/api-http/script.js
+++ javascript-assincrono/api-http/script.js
@@ -1,21 +1,3 @@
-fetch("/dados.json")
-  .then((response) => response.json())
-  .then((corpo) => {
-    corpo.forEach((materia) => {
-      console.log(materia);
-    });
-  });
-
-const configuracoes = {
-  player: "Google API",
-  tempo: 2.5,
-  aula: "2-1 JavaScript",
-  vitalicio: true,
-};
-
-const stringConfig = JSON.stringify(configuracoes);
-console.log(stringConfig);
-
-const mostrarConfig = JSON.parse(stringConfig);
-console.log(mostrarConfig);
-localStorage.configuracoes = stringConfig;
+fetch('https://pokeapi.co/api/v2/pokemon')
+.then(r => r.json())
+.then(r => console.log(r))
