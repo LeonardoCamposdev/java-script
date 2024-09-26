38a3a868f0f0859fb85aeca3db4f6a4ddb8eb5f8
Leonardo Campos <Leonardocamposdev777@gmail.com>
2024-09-25 23:17:44 -0300

    update

--- efeitos-no-dom/animais-fetch/js/modules/fetch-animais.js
+++ efeitos-no-dom/animais-fetch/js/modules/fetch-animais.js
@@ -1,22 +1,20 @@
-export default function initFetchAnimais(){
-
-}
+export default function initFetchAnimais() {}
 
 async function animaisFetch(url) {
   const animaResponse = await fetch(url);
   const animaisJSON = await animaResponse.json();
-  const numerosGrid = document.querySelector('.numeros-grid');
-  animaisJSON.forEach((animal) =>{
+  const numerosGrid = document.querySelector(".numeros-grid");
+  animaisJSON.forEach((animal) => {
     const divAnimal = createAnimal(animal);
     numerosGrid.appendChild(divAnimal);
-  })
+  });
 }
 
-function createAnimal(animal){
-  const div = document.createElement('div');
-  div.classList.add('numero-animal');
-  div.innerHTML = `<h3>${animal.especie}</h3><span data-numero>${animal.total}`
+function createAnimal(animal) {
+  const div = document.createElement("div");
+  div.classList.add("numero-animal");
+  div.innerHTML = `<h3>${animal.especie}</h3><span data-numero>${animal.total}`;
   return div;
 }
 
-animaisFetch('./animaisapi.json');
+animaisFetch("./animaisapi.json");
