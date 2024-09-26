88591399924e5d304369ae6fa15bfd3801c328b4
Leonardo Campos <Leonardocamposdev777@gmail.com>
2024-09-25 22:36:22 -0300

    update

---
+++ efeitos-no-dom/animais-fetch/js/modules/fetch-animais.js
@@ -0,0 +1,22 @@
+export default function initFetchAnimais(){
+
+}
+
+async function animaisFetch(url) {
+  const animaResponse = await fetch(url);
+  const animaisJSON = await animaResponse.json();
+  const numerosGrid = document.querySelector('.numeros-grid');
+  animaisJSON.forEach((animal) =>{
+    const divAnimal = createAnimal(animal);
+    numerosGrid.appendChild(divAnimal);
+  })
+}
+
+function createAnimal(animal){
+  const div = document.createElement('div');
+  div.classList.add('numero-animal');
+  div.innerHTML = `<h3>${animal.especie}</h3><span data-numero>${animal.total}`
+  return div;
+}
+
+animaisFetch('./animaisapi.json');
--- javascript-assincrono/history-api/script.js
+++ javascript-assincrono/history-api/script.js
@@ -2,31 +2,29 @@ const links = document.querySelectorAll("a");
 
 function handleClick(event) {
   event.preventDefault();
-  fetchPage(event.target.href); //(ativando a função) pegando a url do (a) que a gente cliar (home ou sobre)
-  window.history.pushState(null, null, event.target.href);
+  const url = event.target.href;
+  pushFetch(url);
+  window.history.pushState(null,null,event.target.href);
 }
 
-async function fetchPage(url) {  //(criando função) pegando o conteudo fetch de ambas as páginas através do url
-  const pageResponse = await fetch(url);
-  const pageText = await pageResponse.text(); //conteudo do body do fetch
-
-  replaceContet(pageText); //(ativando a função) para colocar colocar o texto do fetch na página web
+async function pushFetch(url) {
+  const pageFetch = await fetch(url);
+  const pageText = await pageFetch.text();
+  replaceContent(pageText);
 }
 
-function replaceContet(newText) { //(criando funcção) colocando o conteudo do fetch dentro da página web 
-  const newHtml = document.createElement("div");
-  newHtml.innerHTML = newText; //(conteudo do pageText)
+function replaceContent(newText){
+  const newHtml = document.createElement('div');
+  newHtml.innerHTML = newText;
 
-  const oldContent = document.querySelector(".content");
-  const newContet = newHtml.querySelector(".content");
-
-  oldContent.innerHTML = newContet.innerHTML;
-  document.title = newHtml.querySelector("title").innerText;
+  const oldContent = document.querySelector('.content');
+  const newContent = newHtml.querySelector('.content');
+  oldContent.innerHTML  = newContent.innerHTML;
 }
 
-window.addEventListener("popstate", () => {
-  fetchPage(window.location.href);
-});
+window.addEventListener('popstate', () =>{
+  pushFetch(window.location.pathname);
+})
 
 links.forEach((link) => {
   link.addEventListener("click", handleClick);
