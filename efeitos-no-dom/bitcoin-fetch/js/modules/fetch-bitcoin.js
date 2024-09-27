8d41de9cc50bfaf243939319e97f629b9e9922f0
Leonardo Campos <Leonardocamposdev777@gmail.com>
2024-09-27 19:56:13 -0300

    update

---
+++ efeitos-no-dom/bitcoin-fetch/js/modules/fetch-bitcoin.js
@@ -0,0 +1,21 @@
+export default function initFetchBitcoin() {}
+
+const doeBitcoin = document.querySelector("[data-bitcoin]");
+
+async function bitcoinFetch(url) {
+  try{
+    const bitcoinResponse = await fetch(url);
+    const bitcoinJSON = await bitcoinResponse.json();
+    const bitcoinBuy = (1000 / bitcoinJSON.BRL.sell).toFixed(4);
+    documentBitcoin(bitcoinBuy);
+  }
+  catch(erro){
+    console.log(erro);
+  }
+}
+
+function documentBitcoin(bitcoin) {
+  doeBitcoin.innerText = bitcoin;
+}
+
+bitcoinFetch("https://blockchaain.info/ticker");
