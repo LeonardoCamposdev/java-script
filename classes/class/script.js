9a6925284fed6650bb25d517afb9c985f474f6df
Leonardo Campos <Leonardocamposdev777@gmail.com>
2024-10-11 00:42:05 -0300

    update

--- classes/class/script.js
+++ classes/class/script.js
@@ -1,25 +1,25 @@
 class Button {
-  constructor(text, color, background) {
+  constructor(text, background) {
     this.text = text;
-    this.color = color;
     this.background = background;
   }
 
-  static element() {
-    const btnElement = document.createElement("button");
-    btnElement.innerText = this.text;
-    btnElement.style.color = this.color;
-    btnElement.style.backgroundColor = this.background;
-    btnElement.style.padding = "20px 80px";
-    btnElement.style.textTransform = "uppercase";
-    btnElement.style.fontWeight = "800";
-    btnElement.style.margin = "40px";
-    return btnElement;
+  objetos ={
+    teste1: '1',
+    teste2: '2',
   }
 
+  element() {
+    const btnElement = document.createElement("button");
+    btnElement.innerText = this.text;
+    btnElement.style.backgroundColor = this.background;
+    return btnElement;
+  }
+  static createRed(text){
+    return new Button(text, 'red')
+  }
 }
 
-const button1 = new Button("COMPRAR", "green", "lightblue");
-const button2 = new Button("ALUGAR", "red", "grey");
-const button3 = new Button("VENDER", "purple", "pink");
-console.log(Button.element())
+console.log(Button.createRed('Compre'))
+const newTeste = new Button()
+console.log(newTeste.objetos)
