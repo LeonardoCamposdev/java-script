2ff62f49aa92a59d01384fd9a00346ed5d44803d
Leonardo Souza Campos <leonardokiss845@gmail.com>
2025-03-28 17:00:19 -0300

    up up

---
+++ javascript-intermediario2/loops-&-iterable/script.js
@@ -0,0 +1,36 @@
+const frutas = ['Banana','Morango','Uva'];
+const frase = 'Isso é JavaScript';
+
+
+for(const fruta of frutas){
+  console.log(fruta);
+}
+
+for(const caracter of frase){
+  console.log(caracter);
+}
+
+const buttons = document.querySelectorAll('button');
+
+for(const btn of buttons){
+  btn.style.backgroundColor = 'blue';
+}
+
+console.log(...buttons);
+
+const carro = {
+  marca: 'Honda',
+  ano: 2018,
+}
+
+for(const propiedades in carro){
+  console.log(propiedades, carro[propiedades]);
+}
+
+
+const btnUnico = document.querySelector('button');
+const btnStyles = getComputedStyle(btnUnico);
+
+for(const style in btnStyles){
+  console.log(`${style}`, btnStyles[style]);
+}
