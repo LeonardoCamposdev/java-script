e22d640b5aaa7a0f6f9c4deec4055a7625dcea3d
Leonardo Campos <Leonardocamposdev777@gmail.com>
2025-03-31 22:46:34 -0300

    up

---
+++ EXERCICIOS-JS-INICIANTE/exe-4/script.js
@@ -0,0 +1,19 @@
+const names = [
+  'Hole-in-one', //1
+  'Eagle', // <= par - 2
+  'Birdie', // par - 1
+  'Par', // par = par
+  'Bogey', // par + 1
+  'Double Bogey', // par + 2
+  'Go Home', // >= par + 3
+]
+
+function golfScore(par, strokes){
+
+}
+
+let result = golfScore(6, 10);
+console.log(result);
+
+//par = número ideal para acetar o buraco;
+// strokes= números de tacadas que o participante já deu;
