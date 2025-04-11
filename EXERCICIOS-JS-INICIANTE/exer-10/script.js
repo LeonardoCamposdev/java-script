35160b0d075868f9464174b15c164a0e9a0e3a83
Leonardo Campos <Leonardocamposdev777@gmail.com>
2025-04-11 08:00:00 -0300

    update

--- EXERCICIOS-JS-INICIANTE/exer-10/script.js
+++ EXERCICIOS-JS-INICIANTE/exer-10/script.js
@@ -1,9 +1,9 @@
 function showStars(rows) {
-  let pattern = "";
-
-  for (let i = 0; i < rows; i++) {
-    pattern += "*";
-    console.log(pattern);
+  for (let row = 1; row <= rows; row++) {
+    let pattern = "";
+    for (let i = 0; i < row; i++)
+      pattern += "*";
+      console.log(pattern);
   }
 }
 
