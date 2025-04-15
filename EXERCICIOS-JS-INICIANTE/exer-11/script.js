02b5733ac7396288d8a31a91caeeb2dd8401c766
Leonardo Souza Campos <leonardokiss845@gmail.com>
2025-04-15 10:47:06 -0300

    Update script.js

--- EXERCICIOS-JS-INICIANTE/exer-11/script.js
+++ EXERCICIOS-JS-INICIANTE/exer-11/script.js
@@ -1,5 +1,12 @@
 const arr = [[1,2], [3,4], [5,6]];
 
+
+arr.forEach((elemento)=>{
+    elemento.forEach((item)=>{
+        let multipliqueItens = item *=  item;
+        console.log(multipliqueItens)
+    })
+})
 function multipleAll(arr){
 
 }
