c3a7389c9ddc94cfa80ac68aae7bb0e3ac3aeb0d
Leonardo Campos <Leonardocamposdev777@gmail.com>
2025-04-14 22:56:28 -0300

    uu

--- EXERCICIOS-JS-INICIANTE/exer-11/script.js
+++ EXERCICIOS-JS-INICIANTE/exer-11/script.js
@@ -1,7 +1,14 @@
 const arr = [[1,2], [3,4], [5,6]];
 
 function multipleAll(arr){
+  let result = 1;
 
+  for (let i = 0; i < arr.length; i++){
+    for (let j = 0; j < arr[i].length; j++){
+      result *= arr[i][j];
+    }
+  }
+  return result;
 }
 
 console.log(multipleAll(arr))
