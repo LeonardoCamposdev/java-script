f821bac5bdb5e3313a7a29468ea480fd4a105353
Leonardo Souza Campos <leonardokiss845@gmail.com>
2025-04-30 16:37:30 -0300

    Update script.js

--- EXERCICIOS-JS-APLICADO-AO-DOM/exe-4/script.js
+++ EXERCICIOS-JS-APLICADO-AO-DOM/exe-4/script.js
@@ -1,16 +1,31 @@
 const btnNext = document.querySelector('#nextSlide');
 const btnPrevious = document.querySelector('#previousSlide');
 const slider = document.querySelector('.slider');
+const content = document.querySelector('.content')
 
-const {width: sliderWidth} = window.getComputedStyle(slider);
+const {width: slideWidth} = window.getComputedStyle(slider);
+const {width: contentWidth} = window.getComputedStyle(content)
+
+const slideProps = {
+  width: parseInt(slideWidth),
+  scroll: 0,
+}
 
 function controlSlide({ target: {id}}){
   switch (id) {
-    case 'nextSlide':
-    return slider.scrollLeft += parseInt(sliderWidth);
+    case 'nextSlide':{
+      if(slideProps.scroll + slideProps.width < parseInt(contentWidth)){
+        slideProps.scroll += slideProps.width;
+      }
+      return slider.scrollLeft = slideProps.scroll;
+    }
+
       
-    case 'previousSlide':
-      return slider.scrollLeft -= parseInt(sliderWidth);
+    case 'previousSlide':{
+      slideProps.scroll = slideProps.scroll - slideProps.width < 0 ? 0 : slideProps.scroll - slideProps.width;
+      return slider.scrollLeft = slideProps.scroll;
+    }
+
 
     default:
       break;
