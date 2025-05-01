36da02cf15e599383195c978274c657c8416e49e
Leonardo Campos <Leonardocamposdev777@gmail.com>
2025-04-30 22:46:52 -0300

    up

--- EXERCICIOS-JS-APLICADO-AO-DOM/exe-4/script.js
+++ EXERCICIOS-JS-APLICADO-AO-DOM/exe-4/script.js
@@ -1,19 +1,19 @@
-const btnNext = document.querySelector("#nextSlide");
-const btnPrevious = document.querySelector("#previousSlide");
-const slider = document.querySelector(".slider");
-const content = document.querySelector(".content");
+const btnNext = document.querySelector("#nextSlide"); //1 
+const btnPrevious = document.querySelector("#previousSlide"); //1 
+const slider = document.querySelector(".slider"); //1
+const content = document.querySelector(".content"); //2
 
 const { width: slideWidth } = window.getComputedStyle(slider);
 const { width: contentWidth } = window.getComputedStyle(content);
 
-let currentSlide = 0;
+let currentSlide = 0; //2
 
-const slideProps = {
+const slideProps = { // 1
   width: parseInt(slideWidth),
   scroll: 0,
 };
 
-function setCurrentDot() {
+function setCurrentDot() { //3
   //FUNÇÃO PARA DEIXAR A BOLINHA AZUL DE ACORDO COM QUAL IMAGEM ESTÁ NA TELA
   const dots = document.querySelectorAll(".dot");
   for (let dot of dots) {
@@ -22,34 +22,34 @@ function setCurrentDot() {
   dots[currentSlide].classList.add("current");
 }
 
-function controlSlide({ target: { id } }) {
-  const contentLength = content.children.length;
-  switch (id) {
+function controlSlide({ target: { id } }) { //1
+  const contentLength = content.children.length; //2
+  switch (id) { //1
     case "nextSlide": {
-      if (slideProps.scroll + slideProps.width < parseInt(contentWidth)) {
+      if (slideProps.scroll + slideProps.width < parseInt(contentWidth)) { //1
         slideProps.scroll += slideProps.width;
       }
-      if (currentSlide < contentLength - 1) {
+      if (currentSlide < contentLength - 1) { //2
         //PARA AS BOLINHAS NÃO DAREM ERRO AO PASSAR DA ULTIMA IMAGEM O CLICK
         currentSlide += 1;
-        setCurrentDot();
+        setCurrentDot(); //3
       }
 
-      return (slider.scrollLeft = slideProps.scroll);
+      return (slider.scrollLeft = slideProps.scroll); //1 //O SCROLL DO SLIDER O VALOR VAI SER OQUE A GENTE DEFINIU NO (SLIDERPROPS.SCROLL)
     }
 
-    case "previousSlide": {
-      if (currentSlide > 0) {
+    case "previousSlide": { //1
+      if (currentSlide > 0) { //2
         //PARA AS BOLINHAS NÃO DAREM ERRO AO VOLTAR DA PRIMEIRA IMAGEM O CLICK
         currentSlide -= 1;
-        setCurrentDot();
+        setCurrentDot(); //3
       }
       slideProps.scroll =
         slideProps.scroll - slideProps.width < 0
           ? 0
           : slideProps.scroll - slideProps.width;
 
-      return (slider.scrollLeft = slideProps.scroll);
+      return (slider.scrollLeft = slideProps.scroll); //O SCROLL DO SLIDER O VALOR VAI SER OQUE A GENTE DEFINIU NO (SLIDERPROPS.SCROLL)
     }
 
     default:
@@ -57,16 +57,16 @@ function controlSlide({ target: { id } }) {
   }
 }
 
-btnNext.addEventListener("click", controlSlide);
-btnPrevious.addEventListener("click", controlSlide);
-
+btnNext.addEventListener("click", controlSlide); //1 
+btnPrevious.addEventListener("click", controlSlide); //1
+ 
 // FUNÇÃO PARA CLONAR AS BOLINHAS DE ACORDO COM A QUANTIDADE DE IMAGENS QUE TEM NO SLIDE
-window.onload = () => {
+window.onload = () => { //2 // FUNÃO QUE VAI ACONTECER ASSIM QUE A PÁGINA ABRIR
   const contentLength = content.children.length; //pegando o total de imagens
   for (let index = 0; index < contentLength - 1; index += 1) {
     // o -1 é porque já tem uma bolinha no html!!!
     const newDot = slider.parentElement.querySelector(".dot").cloneNode(); //clonando as bolinhas
     slider.parentElement.querySelector(".length-dots").appendChild(newDot); //colocando as bolinhas uma atrás da outra
   }
-  setCurrentDot();
+  setCurrentDot(); //3
 };
