511fc1b8ca6d5324ab3635bf1eeb50575b43e771
Leonardo Souza Campos <leonardokiss845@gmail.com>
2025-05-06 16:43:36 -0300

    UP

--- EXERCICIOS-JS-APLICADO-AO-DOM/exe-5/script.js
+++ EXERCICIOS-JS-APLICADO-AO-DOM/exe-5/script.js
@@ -1,44 +1,12 @@
-const btns = document.querySelectorAll('button');
-const som1 = new Audio('sounds/boom.wav'); 
-const som2 =  new Audio('sounds/clap.wav'); 
-
-function controlDrum({key}){
-    switch (key) {
-        case 'a':
-            som1.play();
-        break;
-
-        case 's':
-            som2.play();
-        break;
-
-        case 'd':
-            console.log(key)
-        break;
-
-        case 'f':
-            console.log(key)
-        break;
-
-        case 'g':
-            console.log(key)
-        break;
-
-        case 'h':
-            console.log(key)
-        break;
-
-        case 'j':
-            console.log(key)
-        break;
-
-        case 'l':
-            console.log(key)
-        break;
-        default:
-            break;
-    }
+const soundList = {
+    a: 'boom',
+    s: 'clap',
+    d: 'hihat',
+    f: 'kick',
+    g: 'openhat',
+    h: 'ride',
+    j: 'snare',
+    k: 'tink',
+    l: 'tom',
 }
 
-
-window.addEventListener('keydown',controlDrum)
