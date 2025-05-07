f3585715661bb6213288156b6065568ebd80e193
Leonardo Campos <Leonardocamposdev777@gmail.com>
2025-05-06 23:13:26 -0300

    up

--- EXERCICIOS-JS-APLICADO-AO-DOM/exe-5/script.js
+++ EXERCICIOS-JS-APLICADO-AO-DOM/exe-5/script.js
@@ -1,12 +1,21 @@
 const soundList = {
-    a: 'boom',
-    s: 'clap',
-    d: 'hihat',
-    f: 'kick',
-    g: 'openhat',
-    h: 'ride',
-    j: 'snare',
-    k: 'tink',
-    l: 'tom',
+  a: "boom",
+  s: "clap",
+  d: "hihat",
+  f: "kick",
+  g: "openhat",
+  h: "ride",
+  j: "snare",
+  k: "tink",
+  l: "tom",
+};
+
+function playDrum({ key }) {
+  const soundName = soundList[key];
+  if (soundName) {
+    const audio = new Audio(`./sounds/${soundName}.wav`);
+    audio.play();
+  }
 }
 
+window.addEventListener("keydown", playDrum);
