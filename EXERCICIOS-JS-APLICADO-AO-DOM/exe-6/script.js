60e9b360c8178133e346b3a443a57551e6974664
Leonardo Campos <Leonardocamposdev777@gmail.com>
2025-05-08 19:07:16 -0300

    finalizada, calculadora

--- EXERCICIOS-JS-APLICADO-AO-DOM/exe-6/script.js
+++ EXERCICIOS-JS-APLICADO-AO-DOM/exe-6/script.js
@@ -1,11 +1,21 @@
-const calculatorBtns = document.querySelectorAll('[id^="tecla"]');
-console.log(calculatorBtns)
-
-
-function controlCalc({ key }){
-  
+function insertToDisplay(data) {
+  document.querySelector("#display").value += data;
 }
 
-calculatorBtns.forEach((btn)=>{
-  btn.addEventListener('click',controlCalc);
-})
+function clean() {
+  document.querySelector("#display").value = "";
+}
+
+function back() {
+  const display = document.querySelector("#display");
+  display.value = display.value.slice(0, -1);
+}
+
+function result() {
+  const display = document.querySelector("#display");
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = 'Error'
+  }
+}
