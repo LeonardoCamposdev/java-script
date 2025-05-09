d687c18aa13fe6c8922b4cc5465338b49a9fd6f9
Leonardo Souza Campos <leonardokiss845@gmail.com>
2025-05-09 17:11:59 -0300

    calculadora, finalizada

--- EXERCICIOS-JS-APLICADO-AO-DOM/exe-6/script.js
+++ EXERCICIOS-JS-APLICADO-AO-DOM/exe-6/script.js
@@ -1,5 +1,28 @@
+// O ONCLICK NO HTML SIGNIFICA QUE AO CLICARMOS NO ELEMENTO QUE COLOCAMOS O ONCLICK
+// VAI EXECUTAR A FUNÇÃO QUE PASSAMOS DENTRO DESSE ONCLICK
+
 function caltoDisplay(data){
     document.querySelector('#display').value += data;
 }
 
+function back(){
+    const display = document.querySelector('#display');
 
+    display.value = display.value.slice(0, -1);
+}
+
+function clean(){
+    document.querySelector('#display').value = '';
+}
+
+
+function calcResult(){
+    const display = document.querySelector('#display');
+
+    try{
+        display.value = eval(display.value); //esse método consegue entender funções matemáticas.
+    } catch{
+         display.value = 'Error';
+    }
+
+}
