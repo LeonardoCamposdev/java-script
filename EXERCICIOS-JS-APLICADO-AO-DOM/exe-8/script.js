ada9ed7d810b2438bc950ac00a228f6ba2baab59
Leonardo Souza Campos <leonardokiss845@gmail.com>
2025-05-14 12:30:13 -0300

    upp

--- EXERCICIOS-JS-APLICADO-AO-DOM/exe-8/script.js
+++ EXERCICIOS-JS-APLICADO-AO-DOM/exe-8/script.js
@@ -1,11 +1,24 @@
 const btnTask = document.querySelector('#btn-new-task');
 
+const localStorageKey = 'to-do-list-lc'
+
 function newTask(){
    let input = document.querySelector('#input-new-task');
 
    //VALIDAÇÃO!!
    if(!input.value){
     alert('Digite algo para inserir na sua lista!');
+   }
+   
+   //else if()
+
+   else{
+       //increment to localStorege
+       let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+       values.push({
+         name: input.value,
+       })
+       localStorage.setItem(localStorageKey, JSON.stringify(values));
    }
 }
 
