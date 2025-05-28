d99130089164040064441acc9c6e7a772f1f79a8
Leonardo Campos <157067789+LeonardoCamposdev@users.noreply.github.com>
2025-05-27 21:20:37 -0300

    update

--- EXERCICIOS-JS-APLICADO-AO-DOM/exe-9/main.js
+++ EXERCICIOS-JS-APLICADO-AO-DOM/exe-9/main.js
@@ -1,25 +1,58 @@
-
-
 const tempClient = {
-  nome: 'Leonardo',
-  email: 'Leonardo.kiss66@live.com',
-  celular: '87996507215',
-  cidade: 'Buique-PE'
-}
+  nome: "Edileuza",
+  email: "Edileuza.kiss66@live.com",
+  celular: "87996507215",
+  cidade: "Buique-PE",
+};
 
-const setLocalStorage = (dbClient) => localStorage.setItem('dbClient', JSON.stringify(dbClient));
-const getLocalStorage = () => JSON.parse(localStorage.getItem('dbClient')) ?? [];
+const setLocalStorage = (dbClient) => localStorage.setItem("dbClient", JSON.stringify(dbClient));
+const getLocalStorage = () => JSON.parse(localStorage.getItem("dbClient")) ?? [];
 
-const updadeClient = () =>{
-  
-}
-
-//PUXAR
-const readClient = () => getLocalStorage();
-
-//CRIAR
-const createClient = (cliente) =>{
-  const dbClient = getLocalStorage();
-  dbClient.push(cliente)
+const deleteClient = (index) => {
+  const dbClient = restClient();
+  dbClient.splice(index, 1);
   setLocalStorage(dbClient);
+};
+
+const updadeClient = (index, client) => {
+  const dbClient = restClient();
+  dbClient[index] = client;
+  setLocalStorage(dbClient);
+};
+
+const restClient = () => getLocalStorage();
+
+const createClient = (client) => {
+  const dbClient = getLocalStorage();
+  dbClient.push(client);
+  setLocalStorage(dbClient);
+};
+
+const isValidFields = () =>{ //SE O FORMULÁRIO ESTIVER 100% VÁLIDO VAI RETORNAR TRUE OU FALSE
+  return document.getElementById('form').reportValidity();
 }
+
+const salveClient = () =>{
+  if(isValidFields()){ // ATIVANDO A FUNÇÃO E VENDO SE O RESULTADO VAI SER TRUE OU FALSE
+    console.log('cadastrando cliente');
+  }
+}
+
+//INTERAÇÕES COM O LAYOUT
+function openModal() {
+  console.log('aksdoasjdald')
+  const modal = document.querySelector("#modal");
+  modal.classList.add("active");
+}
+
+function closeModal() {
+  const modal = document.querySelector("#modal");
+  modal.classList.remove("active");
+}
+
+//EVENTS
+document.getElementById("cadastrarCliente").addEventListener("click", openModal);
+
+document.getElementById("modalClose").addEventListener("click", closeModal);
+
+document.getElementById("salvar").addEventListener("click", salveClient);
