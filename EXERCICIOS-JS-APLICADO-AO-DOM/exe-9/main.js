8467a38fe7fbfe41f7c8ff9bf55431991b542789
Leonardo Campos <157067789+LeonardoCamposdev@users.noreply.github.com>
2025-05-28 22:50:39 -0300

    upppp

--- EXERCICIOS-JS-APLICADO-AO-DOM/exe-9/main.js
+++ EXERCICIOS-JS-APLICADO-AO-DOM/exe-9/main.js
@@ -1,69 +1,63 @@
-const tempClient = {
+const Tempclient = {
   nome: "Edileuza",
-  email: "Edileuza.kiss66@live.com",
-  celular: "87996507215",
+  email: "Edileuza.maria66@live.com",
+  numero: "87996507215",
   cidade: "Buique-PE",
 };
 
 const setLocalStorage = (dbClient) => localStorage.setItem("dbClient", JSON.stringify(dbClient));
 const getLocalStorage = () => JSON.parse(localStorage.getItem("dbClient")) ?? [];
 
+//CRUD
 
-//DELETAR
-const deleteClient = (index) => {
+const deleteClient = (index) =>{
   const dbClient = restClient();
-  dbClient.splice(index, 1);
+  dbClient.splice(index,1);
   setLocalStorage(dbClient);
-};
+}
 
-//EDITAR
-const updadeClient = (index, client) => {
+const updateClient = (index, client) => {
   const dbClient = restClient();
   dbClient[index] = client;
   setLocalStorage(dbClient);
 };
 
-//PUXAR
+
 const restClient = () => getLocalStorage();
 
-//CRIAR
 const createClient = (client) => {
   const dbClient = getLocalStorage();
   dbClient.push(client);
   setLocalStorage(dbClient);
 };
 
-const isValidFields = () =>{ //SE O FORMULÁRIO ESTIVER 100% VÁLIDO VAI RETORNAR TRUE OU FALSE
-  return document.getElementById('form').reportValidity();
+const isvalidFields = () =>{
+  const form = document.querySelector('#form');
+  return form.reportValidity();
 }
 
-const salveClient = () =>{
-  if(isValidFields()){ // ATIVANDO A FUNÇÃO E VENDO SE O RESULTADO VAI SER TRUE OU FALSE
-    const client = {
-      nome: document.getElementById('nome').value,
-      email: document.getElementById('email').value,
-      celular: document.getElementById('celular').value,
-      cidade: document.getElementById('cidade').value,
-    }
-    createClient(client);
-    console.log('cadastrando cliente');
+//MANIPULANDO O DOM!!
+function saveClient(){
+  if(isvalidFields()){
+    console.log('validando cadastro');
   }
 }
 
-//INTERAÇÕES COM O LAYOUT
-function openModal() {
-  const modal = document.querySelector("#modal");
-  modal.classList.add("active");
+
+function openModal(){
+  const modal = document.querySelector('#modal');
+  modal.classList.add('active');
 }
 
-function closeModal() {
-  const modal = document.querySelector("#modal");
-  modal.classList.remove("active");
+function closeModal(){
+   const modal = document.querySelector('#modal');
+   modal.classList.remove('active');
 }
 
-//EVENTS
-document.getElementById("cadastrarCliente").addEventListener("click", openModal);
 
-document.getElementById("modalClose").addEventListener("click", closeModal);
+//EVENTS!!!!
+document.getElementById('cadastrarCliente').addEventListener('click',openModal);
 
-document.getElementById("salvar").addEventListener("click", salveClient);
+document.getElementById('modalClose').addEventListener('click',closeModal);
+
+document.getElementById('salvar').addEventListener('click',saveClient);
