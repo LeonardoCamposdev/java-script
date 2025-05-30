9319cc395484cbc130d671da0e13523fd34be770
Leonardo Campos <157067789+LeonardoCamposdev@users.noreply.github.com>
2025-05-30 20:03:58 -0300

    up

--- EXERCICIOS-JS-APLICADO-AO-DOM/exe-9/main.js
+++ EXERCICIOS-JS-APLICADO-AO-DOM/exe-9/main.js
@@ -1,29 +1,23 @@
-const Tempclient = {
-  nome: "Edileuza",
-  email: "Edileuza.maria66@live.com",
-  numero: "87996507215",
-  cidade: "Buique-PE",
-};
-
-const setLocalStorage = (dbClient) => localStorage.setItem("dbClient", JSON.stringify(dbClient));
-const getLocalStorage = () => JSON.parse(localStorage.getItem("dbClient")) ?? [];
+const setLocalStorage = (dbClient) =>
+  localStorage.setItem("dbClient", JSON.stringify(dbClient));
+const getLocalStorage = () =>
+  JSON.parse(localStorage.getItem("dbClient")) ?? [];
 
 //CRUD
 
-const deleteClient = (index) =>{
-  const dbClient = restClient();
-  dbClient.splice(index,1);
+const deleteClient = (index) => {
+  const dbClient = readClient();
+  dbClient.splice(index, 1);
   setLocalStorage(dbClient);
-}
+};
 
 const updateClient = (index, client) => {
-  const dbClient = restClient();
+  const dbClient = readClient();
   dbClient[index] = client;
   setLocalStorage(dbClient);
 };
 
-
-const restClient = () => getLocalStorage();
+const readClient = () => getLocalStorage();
 
 const createClient = (client) => {
   const dbClient = getLocalStorage();
@@ -31,47 +25,80 @@ const createClient = (client) => {
   setLocalStorage(dbClient);
 };
 
-const isvalidFields = () =>{
-  return document.getElementById('form').reportValidity();
-}
+const isvalidFields = () => {
+  return document.getElementById("form").reportValidity();
+};
 
 //MANIPULANDO O DOM!!
-const clearFields = () =>{
-  const modalField = document.querySelectorAll('.modal-field');
-  modalField.forEach((field) =>{
-    field.value = '';
-  })
-}
+const clearFields = () => {
+  const modalField = document.querySelectorAll(".modal-field");
+  modalField.forEach((field) => {
+    field.value = "";
+  });
+};
 
-function saveClient(){
-  if(isvalidFields()){
+function saveClient() {
+  if (isvalidFields()) {
     const client = {
-      nome: document.getElementById('nome').value,
-      email: document.getElementById('email').value,
-      celular: document.getElementById('celular').value,
-      cidade: document.getElementById('cidade').value,
-    }
+      nome: document.getElementById("nome").value,
+      email: document.getElementById("email").value,
+      celular: document.getElementById("celular").value,
+      cidade: document.getElementById("cidade").value,
+    };
     createClient(client);
+    updadeTable();
     closeModal();
   }
 }
 
+const createRow = (client) => {
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = `
+                      <td>${client.nome}</td>
+                    <td>${client.email}</td>
+                    <td>${client.celular}</td>
+                    <td>${client.cidade}</td>
+                    <td>
+                        <button type="button" class="button green">editar</button>
+                        <button type="button" class="button red">excluir</button>
+                    </td>
+  `;
+  const tbody = document.createElement('tbody');
+  document.getElementById('table').appendChild(tbody);
+  tbody.appendChild(newRow);
+};
 
-function openModal(){
-  const modal = document.querySelector('#modal');
-  modal.classList.add('active');
+const clearTable = () =>{
+  const rows = document.querySelectorAll("tr")
+  rows.forEach((row) =>{
+    row.innerHTML = '';
+  });
+  return rows;
 }
 
-function closeModal(){
-   clearFields();
-   const modal = document.querySelector('#modal');
-   modal.classList.remove('active');
+const updadeTable = () => {
+  const dbClient = readClient();
+  clearTable();
+  dbClient.forEach(createRow);
+};
+
+function openModal() {
+  const modal = document.querySelector("#modal");
+  modal.classList.add("active");
 }
 
+function closeModal() {
+  clearFields();
+  const modal = document.querySelector("#modal");
+  modal.classList.remove("active");
+}
 
+updadeTable();
 //EVENTS!!!!
-document.getElementById('cadastrarCliente').addEventListener('click',openModal);
+document
+  .getElementById("cadastrarCliente")
+  .addEventListener("click", openModal);
 
-document.getElementById('modalClose').addEventListener('click',closeModal);
+document.getElementById("modalClose").addEventListener("click", closeModal);
 
-document.getElementById('salvar').addEventListener('click',saveClient);
+document.getElementById("salvar").addEventListener("click", saveClient);
