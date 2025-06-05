545a8f749411e2a43a943f34aa9b18bca99681b0
Leonardo Campos <157067789+LeonardoCamposdev@users.noreply.github.com>
2025-06-04 23:32:54 -0300

    update

---
+++ EXERCICIOS-JS-APLICADO-AO-DOM/exe-9(meu)/main.js
@@ -0,0 +1,61 @@
+
+const setLocalStorage = (dbClient) =>
+  localStorage.setItem("dbClient", JSON.stringify(dbClient));
+const getLocalStorage = () =>
+  JSON.parse(localStorage.getItem("dbClient")) ?? [];
+
+const deleteClient = (index) => {
+  const dbClient = readClient();
+  dbClient.splice(index, 1);
+  setLocalStorage(dbClient);
+};
+//CRUD
+const updadeClient = (index, client) => {
+  const dbClient = readClient();
+  dbClient[index] = client;
+  setLocalStorage(dbClient);
+};
+
+const readClient = () => getLocalStorage();
+
+const createClient = (client) => {
+  const dbClient = getLocalStorage();
+  dbClient.push(client);
+  setLocalStorage(dbClient);
+};
+//CRUD
+
+const isvalidFields = () =>{
+  return document.getElementById('form').reportValidity();
+}
+//MANIPULANDO O DOM
+const saveClient = () =>{
+  if(isvalidFields()){
+    const client = {
+      nome: document.getElementById('nome').value,
+      email: document.getElementById('email').value,
+      celular: document.getElementById('celular').value,
+      cidade: document.getElementById('cidade').value,
+    }
+    createClient(client);
+  } else{
+    console.log('errou')
+  }
+}
+
+const openModal = () =>{
+  const modal = document.querySelector('#modal');
+  modal.classList.add('active');
+}
+
+const closeModal = () =>{
+  const modal = document.querySelector('#modal');
+  modal.classList.remove('active');
+}
+
+//EVENTS
+document.getElementById('cadastrarCliente').addEventListener('click',openModal);
+
+document.getElementById('modalClose').addEventListener('click',closeModal);
+
+document.getElementById('salvar').addEventListener('click',saveClient)
