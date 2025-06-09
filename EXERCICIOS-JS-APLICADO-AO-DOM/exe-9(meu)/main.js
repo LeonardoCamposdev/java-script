59185deea277cc8382b0ecf2d88cdc033de2bfb8
Leonardo Campos <157067789+LeonardoCamposdev@users.noreply.github.com>
2025-06-09 08:07:46 -0300

    upp

--- EXERCICIOS-JS-APLICADO-AO-DOM/exe-9(meu)/main.js
+++ EXERCICIOS-JS-APLICADO-AO-DOM/exe-9(meu)/main.js
@@ -1,4 +1,3 @@
-
 const setLocalStorage = (dbClient) =>
   localStorage.setItem("dbClient", JSON.stringify(dbClient));
 const getLocalStorage = () =>
@@ -25,34 +24,41 @@ const createClient = (client) => {
 };
 //CRUD
 
-const isvalidFields = () =>{
-  return document.getElementById('form').reportValidity();
-}
+const isvalidFields = () => {
+  return document.getElementById("form").reportValidity();
+};
 
-const clearFields = () =>{
-  const modalFiled = document.querySelectorAll('.modal-field');
-  modalFiled.forEach((field) =>{
-    field.value = '';
-  })
-}
+const clearFields = () => {
+  const modalFiled = document.querySelectorAll(".modal-field");
+  modalFiled.forEach((field) => {
+    field.value = "";
+  });
+};
 //MANIPULANDO O DOM
-const saveClient = () =>{
-  if(isvalidFields()){
+const saveClient = () => {
+  if (isvalidFields()) {
     const client = {
-      nome: document.getElementById('nome').value,
-      email: document.getElementById('email').value,
-      celular: document.getElementById('celular').value,
-      cidade: document.getElementById('cidade').value,
+      nome: document.getElementById("nome").value,
+      email: document.getElementById("email").value,
+      celular: document.getElementById("celular").value,
+      cidade: document.getElementById("cidade").value,
+    };
+    const index = document.getElementById("nome").dataset.index;
+    if (index == "new") {
+      createClient(client);
+      updadeTable();
+      clearFields();
+      closeModal();
+    } else{
+      updadeClient(index,client);
+      updadeTable();
+      closeModal();
     }
-    createClient(client);
-    updadeTable();
-    clearFields();
-    closeModal();
   }
-}
+};
 
-const createRow = (client,index) =>{
-  const row = document.createElement('tr');
+const createRow = (client, index) => {
+  const row = document.createElement("tr");
   row.innerHTML = `
          <td>${client.nome}</td>
                     <td>${client.email}</td>
@@ -62,46 +68,68 @@ const createRow = (client,index) =>{
                         <button type="button" class="button green" id='edit-${index}'>Editar</button>
                         <button type="button" class="button red" id='delete-${index}'>Excluir</button>
                     </td>
-  `
-  const tbody = document.querySelector('tbody');
+  `;
+  const tbody = document.querySelector("tbody");
   tbody.appendChild(row);
-}
+};
 
-const clearTable = () =>{
-  const rows = document.querySelectorAll('tbody>tr');
-  rows.forEach(row => row.parentNode.removeChild(row));
-}
+const clearTable = () => {
+  const rows = document.querySelectorAll("tbody>tr");
+  rows.forEach((row) => row.parentNode.removeChild(row));
+};
 
-const updadeTable = () =>{
+const updadeTable = () => {
   const dbClient = readClient();
   clearTable();
   dbClient.forEach(createRow);
-}
+};
 
-const editDelete = (event) =>{
-  if(event.target.type === 'button'){
-    console.log(event.target.id.split('-'))
+const fillFields = (client) => {
+  document.getElementById("nome").value = client.nome;
+  document.getElementById("email").value = client.email;
+  document.getElementById("celular").value = client.celular;
+  document.getElementById("cidade").value = client.cidade;
+  document.getElementById('nome').dataset.index = client.index;
+};
+
+const editClient = (index) => {
+  const client = readClient()[index];
+  client.index = index;
+  fillFields(client);
+  openModal();
+};
+
+const editDelete = (event) => {
+  if (event.target.type === "button") {
+    const [action, index] = event.target.id.split("-");
+
+    if (action == "edit") {
+      editClient(index);
+    } else {
+      console.log("deletando o cliente");
+    }
   }
+};
 
-}
+const openModal = () => {
+  const modal = document.querySelector("#modal");
+  modal.classList.add("active");
+};
 
-const openModal = () =>{
-  const modal = document.querySelector('#modal');
-  modal.classList.add('active');
-}
-
-const closeModal = () =>{
-  const modal = document.querySelector('#modal');
-  modal.classList.remove('active');
-}
+const closeModal = () => {
+  const modal = document.querySelector("#modal");
+  modal.classList.remove("active");
+};
 
 updadeTable();
 
 //EVENTS
-document.getElementById('cadastrarCliente').addEventListener('click',openModal);
+document
+  .getElementById("cadastrarCliente")
+  .addEventListener("click", openModal);
 
-document.getElementById('modalClose').addEventListener('click',closeModal);
+document.getElementById("modalClose").addEventListener("click", closeModal);
 
-document.getElementById('salvar').addEventListener('click',saveClient);
+document.getElementById("salvar").addEventListener("click", saveClient);
 
-document.querySelector('tbody').addEventListener('click', editDelete);
+document.querySelector("tbody").addEventListener("click", editDelete);
