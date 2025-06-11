9b47c7e9620df58fa887530c1a27d10f4c423921
Leonardo Campos <157067789+LeonardoCamposdev@users.noreply.github.com>
2025-06-10 22:45:57 -0300

    up

--- teste/script.js
+++ teste/script.js
@@ -1,1 +1,10 @@
+    const container = document.getElementById("container");
+    const iframe = container.querySelector("iframe");
 
+    container.addEventListener("mouseenter", () => {
+      iframe.classList.add("active");
+    });
+
+    container.addEventListener("mouseleave", () => {
+      iframe.classList.remove("active");
+    });
